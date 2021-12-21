61c1adf16baa69f36bf72850e3268277b6961e2c
Nisha453 <nishasajidahmed.mans@ucalgary.ca>
2021-12-20 22:58:55 -0700

    solve the error in registeruse.php

--- registeruser.php
+++ registeruser.php
@@ -1,8 +1,9 @@
 <?php require_once('dbconnection.php'); ?>
 <?php
       // print_r($_POST);
-      $sql = "SELECT users-id FROM users where email='".$_POST['email']."' AND password='".$_POST['password']."'";
+      $sql = "SELECT user_id FROM users where email='".$_POST['email']."' AND password='".$_POST['password']."'";
       $result = DBConnect::getInstance()->query($sql);
+      if($result){
       if ($result->num_rows <= 0) {
         $sql = "INSERT INTO users (name,address,email,password,phone_number) VALUES ('".$_POST['name']."','".$_POST['address']."','".$_POST['email']."','".$_POST['password']."',".$_POST['number'].")";
         $result = DBConnect::getInstance()->query($sql);
@@ -10,7 +11,8 @@
         $result = DBConnect::getInstance()->query($sql);
         $last_id = mysqli_fetch_array($result)['user_id'];
         echo $last_id;
-      } else{
+      } }
+      else{
         echo null;
       }
 
